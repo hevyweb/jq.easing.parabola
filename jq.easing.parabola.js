2928606952f5495fec5b5378fd17365894b6a847
Dmytro Dzyuba <dmytro.dzyuba@edvantis.com>
2017-11-10 18:22:42 +0200

    Added easing

---
+++ jq.easing.parabola.js
@@ -0,0 +1,45 @@
+/*
+ * jQuery Easing v1.0 - https://github.com/hevyweb/jq.easing.parabola
+ *
+ * This is a jQuery easing which in a form of parabold
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Dmytro Dzyuba
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+*/
+
+$.extend($.easing,
+    {
+        /**
+         * Change data according to the parabolla
+         *
+         * @param float timeDelta is time/duration
+         * @param number time milliseconds from the beggining of the animation
+         * @param number initialValue initial parameter value
+         * @param number delta difference between final value and initial value
+         * @param number duration time in milliseconds - animation duration
+         * @returns number
+         */
+        parabola: function (timeDelta, time, initialValue, delta, duration) {
+            return initialValue + delta * Math.sqrt(1 - Math.pow((2 * timeDelta - 1), 2));
+        }
+    });
